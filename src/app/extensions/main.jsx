3d4d09c2359a68664c7297a6844bc2ca0939b417
Nick Mendes <nicolasmendes_developer@outlook.com>
2024-08-27 22:32:20 -0300

    reverting back to test UI

--- src/app/extensions/main.jsx
+++ src/app/extensions/main.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Table, TableHead, TableRow, TableHeader, TableBody, TableCell, Text, Flex, Input, Divider, LoadingSpinner } from "@hubspot/ui-extensions";
+import React, { useState } from "react";
+import { Button, Text, Flex, Tag, hubspot, LoadingSpinner, Table, TableHead, TableRow, TableHeader, TableBody, TableCell, Heading, Link, Modal, ModalBody } from "@hubspot/ui-extensions";
 import axios from 'axios';
 
 // Define the extension to be run within the Hubspot CRM
@@ -13,85 +13,38 @@ hubspot.extend(({ context, runServerlessFunction, actions }) => (
 
 // Define the Extension component, taking in runServerless, context, & sendAlert as props
 const Extension = ({ context, runServerless, sendAlert }) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post('http://localhost:3000/graphql', {
-          query: `
-            query {
-              availabilityItems {
-                _id
-                product {
-                  name
-                }
-                location {
-                  name
-                }
-                vertical {
-                  name
-                }
-                quantity
-                startDate
-                endDate
-              }
-            }
-          `
-        });
-        setData(response.data.data.availabilityItems);
-        setLoading(false);
-      } catch (error) {
-        sendAlert("error", "Failed to fetch data from GraphQL API");
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [sendAlert]);
-
-  if (loading) {
-    return <LoadingSpinner />;
-  }
+  const [text, setText] = useState("");
+  const fallbackLocal = 'Home';
+  const fallbackVertical = 'Banner';
+  const fallbackProduto = 'Regular';
+  const fallbackDescricao = 'Lorem ipsum dolor conecster amett adhet';
 
   return (
     <>
-      <Text>
-        <Text format={{ fontWeight: "bold" }}>
-          This is the inventory engine front-end made by Epic Digital.
-        </Text>
-        Hi, {context.user.firstName}, this is under development and will be available soon to the iFood Sales Team, please wait a little longer
-      </Text>
-      <Flex direction="row" align="end" gap="small">
-        <Input name="text" label="Send" onInput={(t) => setText(t)} />
-      </Flex>
       <Divider />
-      <Table bordered={true} paginated={true} pageCount="5">
-        <TableHead>
-          <TableRow>
+      <Table 
+        bordered={true}
+        paginated={true}
+        pageCount="5"
+      >
+      <TableHead>
+        <TableRow>
             <TableHeader>LOCAL</TableHeader>
             <TableHeader>VERTICAL</TableHeader>
             <TableHeader>PRODUTO</TableHeader>
-            <TableHeader>QUANTIDADE</TableHeader>
-            <TableHeader>INÍCIO</TableHeader>
-            <TableHeader>FIM</TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item, index) => (
-            <TableRow key={index}>
-              <TableCell>{item.location.name}</TableCell>
-              <TableCell>{item.vertical.name}</TableCell>
-              <TableCell>{item.product.name}</TableCell>
-              <TableCell>{item.quantity}</TableCell>
-              <TableCell>{item.startDate}</TableCell>
-              <TableCell>{item.endDate}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+            <TableHeader>TIPO</TableHeader>
+            <TableHeader>DESCRIÇÃO</TableHeader>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+            <TableCell>{ fallbackLocal }</TableCell>
+            <TableCell>{ fallbackVertical }</TableCell>
+            <TableCell>{ fallbackProduto }</TableCell>
+            <TableCell>{ fallbackDescricao }</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
     </>
   );
 };
